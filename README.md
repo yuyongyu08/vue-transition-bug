f63dbbe3f28efc04a3d9aae7308f72a5198e04a6
yuyongyu <yuyongyu@jwzg.com>
2023-03-08 00:07:14 +0800

    add header

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # vue-project
 
 本工程主要是用于演示使用<transition>时遇到的奇怪问题，具体的原因参见个人博客：[入口](https://juejin.cn/user/3509296844248375/posts)
+
 ## Project Setup
 
 ```sh
-npm install
+yarn install
 ```
 
 ### Compile and Hot-Reload for Development
 
 ```sh
-npm run dev
+yarn run dev
 ```
 
 ### Compile and Minify for Production
 
 ```sh
-npm run build
+yarn run build
 ```
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <a v-for="r in routes" :href="r.path">{{ r.path }}</a>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import router from "../router";
+let routes = ref(router.options.routes[0].children);
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
